4b91e3555922dd0edc010c643d8a6d6ae57351f8
Dan Le <dan@jjumpp.com>
2018-03-08 21:18:21 -0700

    add test for async

--- test/controllers/authController.spec.js
+++ test/controllers/authController.spec.js
@@ -1,14 +1,27 @@
 let assert = require('assert');
 let authController = require('../../controllers/authController');
 
-describe('authController', () => {
-	describe('isAuthorized', () => {
-		it('Should return false if not authorized', () => {
-			assert.equal(false, authController.isAuthorized(['user'], 'admin'))
+describe('authController', function () {
+	beforeEacb(function() {
+		authController.setRoles(['user'])
+	})
+
+	describe('isAuthorized', function () {
+		it('Should return false if not authorized', function () {
+			assert.equal(false, authController.isAuthorized(['admin']))
 		})
 
-		it('Should return true if is authorized', () => {
+		it('Should return true if is authorized', function () {
 			assert.equal(true, authController.isAuthorized(['user', 'admin'], 'admin'))
+		})
+	})
+
+	describe('isAuthorizedAsync', function () {
+		it('Should return false if not authorized', function (done) {
+			authController.isAuthorizedAsync(['user'], 'admin', function (isAuth) {
+				assert.equal(false, isAuth);
+				done();
+			})
 		})
 	})
 });
